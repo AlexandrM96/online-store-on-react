804e72f0b3a4acfa1cbb148883c52853a5313e12
AlexandrM96 <super.dwtd2013@yandex.ru>
2022-04-12 20:11:42 +0300

    сверстал блок SpecialOffer

--- src/components/SpecialOffer/SpecialOffer.js
+++ src/components/SpecialOffer/SpecialOffer.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 import './SpecialOffer.css';
-
+import DRON from './img/pack_anafi_1.svg';
+import CAMERA from './img/463836_preview_1.svg';
 class SpecialOffer extends Component {
     render() {
-        return(
-            <h1>SpecialOffer</h1>
+        return (
+            <section className='special-offer'>
+                <div className='special-offer__container'>
+                    <div className='special-offer__block'>
+                        <div className='special-offer__info'>
+                            <p className='special-offer__par-one'>Get up to 20% off Today Only!</p>
+                            <p className='special-offer__par-two'>THE 4K HDR COMPACT DRONE</p>
+                            <p className='special-offer__par-three'>Show Now</p>
+                        </div>
+                        <div>
+                            <img className='special-offer__img' src={DRON} alt="dron" />
+                        </div>
+                    </div>
+                    <div className='special-offer__block'>
+                        <div className='special-offer__info'>
+                            <p className='special-offer__par-one'>Get up to 30% off Today Only!</p>
+                            <p className='special-offer__par-two'>THE 4K HDR COMPACT Camera</p>
+                            <p className='special-offer__par-three'>Show Now</p>
+                        </div>
+                        <div>
+                            <img className='special-offer__img' src={CAMERA} alt="camera" />
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }

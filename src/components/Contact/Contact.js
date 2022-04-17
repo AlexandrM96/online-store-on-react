886ccb3b35fbc35fc7dac1539a94e35185ee6885
AlexandrM96 <super.dwtd2013@yandex.ru>
2022-04-17 19:40:45 +0300

    сверстал блок Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,23 @@ import './Contact.css';
 class Contact extends Component {
     render() {
         return(
-            <h1>Contact</h1>
+            <section className='contact'>
+                <div className='contact__container'>
+                    <div className='contact__information'>
+                        <h5>User Contact</h5>
+                        <h4>Contact Us for those interested.</h4>
+                        <p>The standard chunk of Lorem Ipsum used since the<br/> is reproduced below for those interested.</p>
+                    </div>
+                    <div className='contact__mail-input-and-button'>
+                        <form>
+                        <input className='contact__mail-input' 
+                        placeholder="Enter your email address"
+                        name="e-mail"/>
+                        <button className='contact__mail-button'>Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
         )
     }
 }

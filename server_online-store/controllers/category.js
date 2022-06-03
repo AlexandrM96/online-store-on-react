50ff76c04d98d52a9de45c28895e283481751d38
AlexandrM96 <super.dwtd2013@yandex.ru>
2022-06-03 20:15:23 +0300

    создание отдельного сервера для магазина

---
+++ server_online-store/controllers/category.js
@@ -0,0 +1,50 @@
+const { request } = require('../app');
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorHandler');
+const Position = require('../models/Positions');
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({ user: req.user.id })
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
+}
+
+module.exports.getById = async function (req, res) {
+    try {
+        const categories = await Category.findById(req.params.id)
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
+}
+
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({ _id: req.params.id });
+        await Position.remove({ category: req.params.id });
+        res.status(200).json({
+            message: 'Категория удалена'
+        })
+    } catch (e) {
+        errorHandler(res, e);
+    }
+}
+
+module.exports.create = async function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e);
+    }
+}
+
+module.exports.update = function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(res, e);
+    }
+}
